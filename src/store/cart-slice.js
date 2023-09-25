500da4ed9ecbd481fd23251c1751723835ee7e99
senior_Ori <ori8834@gmail.com>
2023-09-25 14:22:14 +0300

    287. Refresher / Practice: Part 2/2
    287. Refresher / Practice: Part 2/2

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -13,7 +13,7 @@ const cartSlice = createSlice({
       state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -29,10 +29,10 @@ const cartSlice = createSlice({
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
       if (existingItem.quantity === 1) {
-        state.items.filter((item) => item.id !== id);
+        state.items = state.items.filter((item) => item.id !== id);
       } else {
         existingItem.quantity--;
-        //existingItem.totalPrice = existingItem.totalPrice - newItem.price;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
   },
